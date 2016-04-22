eb17d828a03f37d01af1cea0bb5cfa9b5bf71cbf
Emily Tsai <tsaiemily1@gmail.com>
2016-04-21 23:32:20 -0700

    Undo leader name and password columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421195237) do
+ActiveRecord::Schema.define(version: 20160421225901) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "created_at",   null: false
@@ -25,8 +25,6 @@ ActiveRecord::Schema.define(version: 20160421195237) do
   end
 
   create_table "leaders", force: :cascade do |t|
-    t.string   "name"
-    t.string   "password"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
