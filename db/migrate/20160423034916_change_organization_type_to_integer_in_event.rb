1539f186da5b51fde0d77c15965c5b1d4ede02e6
Emily Tsai <tsaiemily1@gmail.com>
2016-04-24 13:56:13 -0700

    Migration cast to integer

--- db/migrate/20160423034916_change_organization_type_to_integer_in_event.rb
+++ db/migrate/20160423034916_change_organization_type_to_integer_in_event.rb
@@ -1,5 +1,5 @@
 class ChangeOrganizationTypeToIntegerInEvent < ActiveRecord::Migration
   def change
-  	change_column :events, :organization, :integer
+  	change_column :events, :organization, 'integer USING CAST(organization AS integer)'
   end
 end
